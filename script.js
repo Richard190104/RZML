ebf93a94457c885e3110aee89a8c390b0206f9cb
Richard190104 <119693579+Richard190104@users.noreply.github.com>
2024-12-19 11:49:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-async function fetchImages() {
-    try {
-        const response = await fetch('https://richard190104.pythonanywhere.com/api/images');
-        const data = await response.json();
+const supabaseUrl = 'https://zpikwrtdtjglxbhbaecy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwaWt3cnRkdGpnbHhiaGJhZWN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2MDQxODUsImV4cCI6MjA1MDE4MDE4NX0.MzFoRqOn_393c3yMM3D269vAgBKaMx5ZHurD-ggfLws';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-        const gallery = document.getElementById('gallery');
-        console.log(data)
-        data.forEach(image => {
-            const imgElement = document.createElement('img');
-            imgElement.src = image.url;
-            imgElement.alt = image.alt;
-            gallery.appendChild(imgElement);
-        });
-    } catch (error) {
-        console.error('Chyba pri načítaní obrázkov:', error);
-    }
+// Príklad: Získať dáta z tabuľky 'users'
+async function fetchImages() {
+    console.log('ahoj')
+  const { data, error } = await supabase
+    .from('images')
+    .select('*');
+  console.log(data)
+  if (error) {
+    console.error('Chyba:', error);
+  } else {
+    console.log('Dáta:', data);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetchImages();
-});
+fetchImages();
